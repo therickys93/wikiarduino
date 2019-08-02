972ca3f467af43e6a1ec73ff9dc51df29bca9695
Riccardo Crippa <therickys93@gmail.com>
2019-08-02 20:42:44 +0200

    inizio implementazione nuovo pinout

--- lib/Wiki/Wiki.cpp
+++ lib/Wiki/Wiki.cpp
@@ -1,16 +1,21 @@
 #include "Arduino.h"
 #include "Wiki.h"
+#include "WikiUtils.h"
 
 int executeRequest(char *buffer)
 {
     for(int index = 0; index < NUMBER_OF_LED; index++){
         char value = buffer[index];
         if(value == '0'){
-
+            switch_off_led(index);
         } else if(value == '1'){
-
+            switch_on_led(index);
         } else if(value == '2'){
-
+            switch_off_led(index);
+            delay(100);
+            switch_on_led(index);
+            delay(100);
+            switch_off_led(index);
         } else {
             return 1;
         }
--- lib/Wiki/WikiUtils.cpp
+++ lib/Wiki/WikiUtils.cpp
@@ -1,12 +1,38 @@
 #include "Arduino.h"
 #include "WikiUtils.h"
+#include "bits.h"
+
+#define ADDRESS_SHIFT 3U
+
+#define SET_BIT(REG, BIT)   ((REG) |=  (BIT))
+#define CLEAR_BIT(REG, BIT) ((REG) &= ~(BIT))
 
 int switch_on_led(int address)
 {
+    // PORTD
+    uint8_t value = 0;
+    // set address
+    value |= address << ADDRESS_SHIFT;
+    // set data bit
+    SET_BIT(value, BIT_2);
+    // set clock
+    SET_BIT(value, BIT_1);
+    // clear clock
+    CLEAR_BIT(value, BIT_1);
     return 0;
 }
 
 int switch_off_led(int address)
 {
+    // PORTD
+    uint8_t value = 0;
+    // set address
+    value |= address << ADDRESS_SHIFT;
+    // set data bit
+    CLEAR_BIT(value, BIT_2);
+    // set clock bit
+    SET_BIT(value, BIT_1);
+    // clear clock bit
+    CLEAR_BIT(value, BIT_1);
     return 0;
 }
